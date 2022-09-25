321150729e8ecf8bf2f29258bdc925a9c1fb4c5b
LorenzoDv <lorenzo.devichi@hotmail.fr>
2022-09-25 14:28:09 +0200

    add scoreboard

---
+++ js/scoreboard.js
@@ -0,0 +1,172 @@
+
+
+let player = {
+    "Blue Gill": [
+        { "PlayerName": "Exemple 1", "Poid": 8.56 },
+        { "PlayerName": "Exemple 2", "Poid": 9.56 },
+        { "PlayerName": "Exemple 3", "Poid": 10.56 },
+        { "PlayerName": "Exemple 4", "Poid": 10.56 },
+        { "PlayerName": "Exemple 5", "Poid": 10.56 },
+        { "PlayerName": "Exemple 6", "Poid": 10.56 },
+        { "PlayerName": "Exemple 7", "Poid": 10.56 },
+        { "PlayerName": "Exemple 8", "Poid": 10.56 },
+        { "PlayerName": "Exemple 9", "Poid": 10.56 },
+        { "PlayerName": "Exemple 10", "Poid": 10.56 },
+
+    ],
+    "Catfish": [
+        { "PlayerName": "Exemple 1", "Poid": 8.56 },
+        { "PlayerName": "Exemple 2", "Poid": 9.56 },
+        { "PlayerName": "Exemple 3", "Poid": 10.56 },
+        { "PlayerName": "Exemple 4", "Poid": 10.56 },
+        { "PlayerName": "Exemple 5", "Poid": 10.56 },
+        { "PlayerName": "Exemple 6", "Poid": 10.56 },
+        { "PlayerName": "Exemple 7", "Poid": 10.56 },
+        { "PlayerName": "Exemple 8", "Poid": 10.56 },
+        { "PlayerName": "Exemple 9", "Poid": 10.56 },
+        { "PlayerName": "Exemple 10", "Poid": 10.56 },
+    ],
+    "Golden Trout": [
+        { "PlayerName": "Yaggua 1", "Poid": 150 },
+        { "PlayerName": "Exemple 2", "Poid": 9.56 },
+        { "PlayerName": "Exemple 3", "Poid": 10.56 },
+        { "PlayerName": "Exemple 4", "Poid": 10.56 },
+        { "PlayerName": "Exemple 5", "Poid": 10.56 },
+        { "PlayerName": "Exemple 6", "Poid": 10.56 },
+        { "PlayerName": "Exemple 7", "Poid": 10.56 },
+        { "PlayerName": "Exemple 8", "Poid": 10.56 },
+        { "PlayerName": "Exemple 9", "Poid": 10.56 },
+        { "PlayerName": "Exemple 10", "Poid": 10.56 },
+    ],
+    "Kokanee Salmon": [
+        { "PlayerName": "Exemple 1", "Poid": 8.56 },
+        { "PlayerName": "Exemple 2", "Poid": 9.56 },
+        { "PlayerName": "Exemple 3", "Poid": 10.56 },
+        { "PlayerName": "Exemple 4", "Poid": 10.56 },
+        { "PlayerName": "Exemple 5", "Poid": 10.56 },
+        { "PlayerName": "Exemple 6", "Poid": 10.56 },
+        { "PlayerName": "Exemple 7", "Poid": 10.56 },
+        { "PlayerName": "Exemple 8", "Poid": 10.56 },
+        { "PlayerName": "Exemple 9", "Poid": 10.56 },
+        { "PlayerName": "Exemple 10", "Poid": 10.56 },
+    ],
+    "Lake Trout": [
+        { "PlayerName": "Exemple 1", "Poid": 8.56 },
+        { "PlayerName": "Exemple 2", "Poid": 9.56 },
+        { "PlayerName": "Exemple 3", "Poid": 10.56 },
+        { "PlayerName": "Exemple 4", "Poid": 10.56 },
+        { "PlayerName": "Exemple 5", "Poid": 10.56 },
+        { "PlayerName": "Exemple 6", "Poid": 10.56 },
+        { "PlayerName": "Exemple 7", "Poid": 10.56 },
+        { "PlayerName": "Exemple 8", "Poid": 10.56 },
+        { "PlayerName": "Exemple 9", "Poid": 10.56 },
+        { "PlayerName": "Exemple 10", "Poid": 10.56 },
+    ],
+    "Largemouth Bass": [
+        { "PlayerName": "Exemple 1", "Poid": 8.56 },
+        { "PlayerName": "Exemple 2", "Poid": 9.56 },
+        { "PlayerName": "Exemple 3", "Poid": 10.56 },
+        { "PlayerName": "Exemple 4", "Poid": 10.56 },
+        { "PlayerName": "Exemple 5", "Poid": 10.56 },
+        { "PlayerName": "Exemple 6", "Poid": 10.56 },
+        { "PlayerName": "Exemple 7", "Poid": 10.56 },
+        { "PlayerName": "Exemple 8", "Poid": 10.56 },
+        { "PlayerName": "Exemple 9", "Poid": 10.56 },
+        { "PlayerName": "Exemple 10", "Poid": 10.56 },
+    ],
+    "Mountain Whitefish": [
+        { "PlayerName": "Exemple 1", "Poid": 8.56 },
+        { "PlayerName": "Exemple 2", "Poid": 9.56 },
+        { "PlayerName": "Exemple 3", "Poid": 10.56 },
+        { "PlayerName": "Exemple 4", "Poid": 10.56 },
+        { "PlayerName": "Exemple 5", "Poid": 10.56 },
+        { "PlayerName": "Exemple 6", "Poid": 10.56 },
+        { "PlayerName": "Exemple 7", "Poid": 10.56 },
+        { "PlayerName": "Exemple 8", "Poid": 10.56 },
+        { "PlayerName": "Exemple 9", "Poid": 10.56 },
+        { "PlayerName": "Exemple 10", "Poid": 10.56 },
+    ],
+    "Pike": [
+        { "PlayerName": "Exemple 1", "Poid": 8.56 },
+        { "PlayerName": "Exemple 2", "Poid": 9.56 },
+        { "PlayerName": "Exemple 3", "Poid": 10.56 },
+        { "PlayerName": "Exemple 4", "Poid": 10.56 },
+        { "PlayerName": "Exemple 5", "Poid": 10.56 },
+        { "PlayerName": "Exemple 6", "Poid": 10.56 },
+        { "PlayerName": "Exemple 7", "Poid": 10.56 },
+        { "PlayerName": "Exemple 8", "Poid": 10.56 },
+        { "PlayerName": "Exemple 9", "Poid": 10.56 },
+        { "PlayerName": "Exemple 10", "Poid": 10.56 },
+    ],
+    "Rainbow Trout": [
+        { "PlayerName": "Exemple 1", "Poid": 8.56 },
+        { "PlayerName": "Exemple 2", "Poid": 9.56 },
+        { "PlayerName": "Exemple 3", "Poid": 10.56 },
+        { "PlayerName": "Exemple 4", "Poid": 10.56 },
+        { "PlayerName": "Exemple 5", "Poid": 10.56 },
+        { "PlayerName": "Exemple 6", "Poid": 10.56 },
+        { "PlayerName": "Exemple 7", "Poid": 10.56 },
+        { "PlayerName": "Exemple 8", "Poid": 10.56 },
+        { "PlayerName": "Exemple 9", "Poid": 10.56 },
+        { "PlayerName": "Exemple 10", "Poid": 10.56 },
+    ],
+    "Sauger": [
+        { "PlayerName": "Exemple 1", "Poid": 8.56 },
+        { "PlayerName": "Exemple 2", "Poid": 9.56 },
+        { "PlayerName": "Exemple 3", "Poid": 10.56 },
+        { "PlayerName": "Exemple 4", "Poid": 10.56 },
+        { "PlayerName": "Exemple 5", "Poid": 10.56 },
+        { "PlayerName": "Exemple 6", "Poid": 10.56 },
+        { "PlayerName": "Exemple 7", "Poid": 10.56 },
+        { "PlayerName": "Exemple 8", "Poid": 10.56 },
+        { "PlayerName": "Exemple 9", "Poid": 10.56 },
+        { "PlayerName": "Exemple 10", "Poid": 10.56 },
+    ],
+    "Smallmouth Bass": [
+        { "PlayerName": "Exemple 1", "Poid": 8.56 },
+        { "PlayerName": "Exemple 2", "Poid": 9.56 },
+        { "PlayerName": "Exemple 3", "Poid": 10.56 },
+        { "PlayerName": "Exemple 4", "Poid": 10.56 },
+        { "PlayerName": "Exemple 5", "Poid": 10.56 },
+        { "PlayerName": "Exemple 6", "Poid": 10.56 },
+        { "PlayerName": "Exemple 7", "Poid": 10.56 },
+        { "PlayerName": "Exemple 8", "Poid": 10.56 },
+        { "PlayerName": "Exemple 9", "Poid": 10.56 },
+        { "PlayerName": "Exemple 10", "Poid": 10.56 },
+    ],
+    "Yellow Perch": [
+        { "PlayerName": "Exemple 1", "Poid": 8.56 },
+        { "PlayerName": "Exemple 2", "Poid": 9.56 },
+        { "PlayerName": "Exemple 3", "Poid": 10.56 },
+        { "PlayerName": "Exemple 4", "Poid": 10.56 },
+        { "PlayerName": "Exemple 5", "Poid": 10.56 },
+        { "PlayerName": "Exemple 6", "Poid": 10.56 },
+        { "PlayerName": "Exemple 7", "Poid": 10.56 },
+        { "PlayerName": "Exemple 8", "Poid": 10.56 },
+        { "PlayerName": "Exemple 9", "Poid": 10.56 },
+        { "PlayerName": "Exemple 10", "Poid": 10.56 },
+    ]
+}
+
+const blueGill = player["Blue Gill"]
+const Catfish = player["Catfish"]
+const GoldenTrout = player["Golden Trout"]
+const KokaneeSalmon = player["Kokanee Salmon"]
+const LakeTrout = player["Lake Trout"]
+const LargemouthBass = player["Largemouth Bass"]
+const Pike = player["Pike"]
+const RainbowTrout = player["Rainbow Trout"]
+const Sauger = player["Sauger"]
+const SmallmouthBass = player["Smallmouth Bass"]
+const YellowPerch = player["Yellow Perch"]
+
+
+
+
+
+
+
+
+
+
+
